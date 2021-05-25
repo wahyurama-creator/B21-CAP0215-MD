75d57acd90f489c1129ab9f1d904945b51400875
wahyurama-creator <wahyuramas32@gmail.com>
2021-05-25 21:20:37 +0700

    Update API Endpoint Post Register Using JSON Object Create Function Get User By Id in Home View Model

--- app/src/main/java/com/dev/skindec/home/HomeActivity.kt
+++ app/src/main/java/com/dev/skindec/home/HomeActivity.kt
@@ -4,12 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.dev.skindec.databinding.ActivityHomeBinding
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.JsonObject
 
 class HomeActivity : AppCompatActivity() {
 
@@ -36,7 +38,16 @@ class HomeActivity : AppCompatActivity() {
             val age = binding.etAge.text.toString()
             val sex = binding.etGender.text.toString()
 
-            homeViewModel.userRegister(age.toInt(), name, sex)
+            val userObject = JsonObject()
+            userObject.addProperty("age", age)
+            userObject.addProperty("name", name)
+            userObject.addProperty("sex", sex)
+
+            homeViewModel.userRegister(userObject)
+            homeViewModel.user.observe(this, {
+                Log.v("userId: ", it.id.toString())
+            })
+            homeViewModel.getUserById(1)
         }
     }
 
--- app/src/main/java/com/dev/skindec/home/HomeViewModel.kt
+++ app/src/main/java/com/dev/skindec/home/HomeViewModel.kt
@@ -6,13 +6,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dev.skindec.core.data.source.remote.network.ApiConfig
 import com.dev.skindec.core.data.source.remote.response.UserResponse
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
+
     private val _user = MutableLiveData<UserResponse>()
     val user: LiveData<UserResponse> = _user
+
+    private val _userId = MutableLiveData<Int>()
+    val userId: LiveData<Int> = _userId
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
@@ -21,9 +26,9 @@ class HomeViewModel : ViewModel() {
         const val TAG = "MainViewModel"
     }
 
-    fun userRegister(age: Int, name: String, sex: String) {
+    fun userRegister(user: JsonObject) {
         _isLoading.value = true
-        val client = ApiConfig.apiService().register(age, name, sex)
+        val client = ApiConfig.apiService().register(user)
         client.enqueue(object : Callback<UserResponse> {
             override fun onResponse(
                 call: Call<UserResponse>,
@@ -32,6 +37,35 @@ class HomeViewModel : ViewModel() {
                 _isLoading.value = false
                 if (response.isSuccessful) {
                     _user.value = response.body()
+                    _userId.value = response.body()?.id
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(
+                call: Call<UserResponse>,
+                t: Throwable
+            ) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+
+        })
+    }
+
+    fun getUserById(id: Int?) {
+        _isLoading.value = true
+        val client = ApiConfig.apiService().getUser(id!!)
+        client.enqueue(object : Callback<UserResponse> {
+            override fun onResponse(
+                call: Call<UserResponse>,
+                response: Response<UserResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _user.value = response.body()
+                    _userId.value = response.body()?.id
                 } else {
                     Log.e(TAG, "onFailure: ${response.message()}")
                 }
