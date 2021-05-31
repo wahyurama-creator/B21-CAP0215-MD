e93ad0ea77524d921d025278877708fa4538c542
wahyurama-creator <wahyuramas32@gmail.com>
2021-05-31 09:25:04 +0700

    Create Interface Upload Image

--- app/src/main/java/com/dev/skindec/core/data/remote/network/ApiService.kt
+++ app/src/main/java/com/dev/skindec/core/data/remote/network/ApiService.kt
@@ -2,6 +2,7 @@ package com.dev.skindec.core.data.remote.network
 
 import com.dev.skindec.core.data.remote.response.UserResponse
 import com.google.gson.JsonObject
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -20,4 +21,10 @@ interface ApiService {
     fun register(
         @Body requestBody: JsonObject
     ): Call<UserResponse>
+
+    @Multipart
+    @POST("upload")
+    fun uploadImage(
+        @Part image: MultipartBody.Part
+    ): Call<Any>
 }
